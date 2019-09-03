2ea57ba02c1685347b0d0262309c3896cfc91e79
Bejugama Akhil sai <40246735+Akhilsai7@users.noreply.github.com>
2019-09-03 12:23:05 +0530

    Update UserController.java

--- src/main/java/com/mrbs/booking/contoller/UserController.java
+++ src/main/java/com/mrbs/booking/contoller/UserController.java
@@ -29,9 +29,9 @@ import antlr.collections.List;
 @Controller
 public class UserController {
 
-	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	DateTimeFormatter timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	LocalDateTime now = LocalDateTime.now();
-	DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("HH:mm:ss");
+	DateTimeFormatter datestamp = DateTimeFormatter.ofPattern("HH:mm:ss");
 	LocalDateTime nowtime = LocalDateTime.now();
 
 	// This method gets called from UserFunctionalities jsp and is used to request a
@@ -55,11 +55,11 @@ public class UserController {
 					"http://localhost:8080/userfunc/getresource", HttpMethod.GET, null,
 					new ParameterizedTypeReference<ArrayList<String>>() {
 					});
-			ModelAndView mv = new ModelAndView("MeetingRequest.jsp");
+			ModelAndView modelandview = new ModelAndView("MeetingRequest.jsp");
 
-			mv.addObject("meetingrooms", responseEntity.getBody());
-			mv.addObject("resources", responseEntity1.getBody());
-			return mv;
+			modelandview.addObject("meetingrooms", responseEntity.getBody());
+			modelandview.addObject("resources", responseEntity1.getBody());
+			return modelandview;
 		}
 		// This method gets called from UserFunctionalities jsp and is used to cancel a
 		// meeting room.
@@ -71,13 +71,13 @@ public class UserController {
 			String url = "http://localhost:8080/userfunc/check";
 			HttpHeaders requestHeaders = new HttpHeaders();
 			HttpEntity<String> requestEntity = new HttpEntity<>(user, requestHeaders);
-			RestTemplate rt = new RestTemplate();
-			ResponseEntity<ArrayList<MeetingRequest>> status = rt.exchange(url, HttpMethod.POST, requestEntity,
+			RestTemplate rest = new RestTemplate();
+			ResponseEntity<ArrayList<MeetingRequest>> status = rest.exchange(url, HttpMethod.POST, requestEntity,
 					new ParameterizedTypeReference<ArrayList<MeetingRequest>>() {
 					});
-			ModelAndView mv = new ModelAndView("Userrequests.jsp");
-			mv.addObject("meetingrequests", status.getBody());
-			return mv;
+			ModelAndView modelandview = new ModelAndView("Userrequests.jsp");
+			modelandview.addObject("meetingrequests", status.getBody());
+			return modelandview;
 		}
 
 		return null;
@@ -100,8 +100,8 @@ public class UserController {
 		meetingrequest.setEndtime(request.getParameter("endtime"));
 		meetingrequest.setResource(request.getParameter("Resource_name"));
 		meetingrequest.setStatus("NEW");
-		meetingrequest.setTimestamp(dtf.format(now));
-		meetingrequest.setTime(dtf1.format(nowtime));
+		meetingrequest.setTimestamp(timestamp.format(now));
+		meetingrequest.setTime(datestamp.format(nowtime));
 		meetingrequest.setUser((String) session.getAttribute("username"));
 		System.out.println(session.getAttribute("username"));
 
